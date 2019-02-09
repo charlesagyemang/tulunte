e3a46ba3514f3084bbbd85417b01d1ab1ebc0108
charles agyemang <micnkru@gmail.com>
2019-02-09 03:22:22 +0000

    Feat; all 3 levels in a basic form

--- assets/pages/jquery.chartist.init.js
+++ assets/pages/jquery.chartist.init.js
@@ -139,23 +139,23 @@ chart.on('created', function() {
 
 
 
-//Simple line chart
-new Chartist.Line('#simple-line-chart', {
-  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-  series: [
-    [12, 9, 7, 8, 5],
-    [2, 1, 3.5, 7, 3],
-    [1, 3, 4, 5, 6]
-  ]
-}, {
-  fullWidth: true,
-  chartPadding: {
-    right: 40
-  },
-  plugins: [
-    Chartist.plugins.tooltip()
-  ]
-});
+////Simple line chart
+//new Chartist.Line('#simple-line-chart', {
+//  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+//  series: [
+//    [12, 9, 7, 8, 5],
+//    [2, 1, 3.5, 7, 3],
+//    [1, 3, 4, 5, 6]
+//  ]
+//}, {
+//  fullWidth: true,
+//  chartPadding: {
+//    right: 40
+//  },
+//  plugins: [
+//    Chartist.plugins.tooltip()
+//  ]
+//});
 
 
 
