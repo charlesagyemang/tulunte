a1e4c94b60feb32f112c030bdf908787d96ab213
charles agyemang <micnkru@gmail.com>
2019-02-22 01:09:32 +0000

    Feat: Make al;l grap and logi workj without the 24 hour check

--- assets/pages/jquery.chartist.init.js
+++ assets/pages/jquery.chartist.init.js
@@ -558,20 +558,6 @@ new Chartist.Bar('#extreme-chart', {
 
 
 
-//Distributed series
-
-new Chartist.Bar('#distributed-series', {
-  labels: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-  series: [20, 60, 120, 200, 180, 20, 10]
-}, {
-  distributeSeries: true,
-  plugins: [
-    Chartist.plugins.tooltip()
-  ]
-});
-
-
-
 //Label placement
 
 new Chartist.Bar('#label-placement-chart', {
